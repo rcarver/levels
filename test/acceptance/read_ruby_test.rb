c9bfd8bbe3ff357c99ed73145b98ee45a5bef935
Ryan Carver <ryan@ryancarver.com>
2012-10-30 07:16:09 -0700

    basic acceptance test for reading ruby

--- test/acceptance/read_ruby_test.rb
+++ test/acceptance/read_ruby_test.rb
@@ -2,7 +2,26 @@ require 'helper'
 
 describe "acceptance: read ruby" do
 
-  it "should have tests" do
-    skip "test me"
+  describe "types" do
+
+    let(:ruby) {
+      <<-RUBY
+set :types,
+    string: "hello",
+    integer: 123,
+    float: 1.5,
+    boolean_true: true,
+    boolean_false: false,
+    array_of_string: ["a", "b", "c"],
+    array_of_integer: [1, 2, 3],
+    null: nil
+set :group2,
+    message: "hello world"
+      RUBY
+    }
+
+    subject { Levels.read_ruby("the ruby", ruby, "file.rb") }
+
+    assert_sample_data_set
   end
 end
