25dd2902496a1cafa498005def793ad32ab1b311
Ryan Carver <ryan@ryancarver.com>
2012-10-30 21:37:40 -0700

    show that ruby supports computed values

--- test/acceptance/read_ruby_test.rb
+++ test/acceptance/read_ruby_test.rb
@@ -24,4 +24,32 @@ set :group2,
 
     assert_sample_data_set
   end
+
+  describe "computed values" do
+
+    let(:level1_ruby) {
+      <<-RUBY
+set :names,
+    full_name: -> { [names.first_name, names.last_name].join(" ") },
+    first_name: "John",
+    last_name: "Doe"
+      RUBY
+    }
+
+    let(:level2_ruby) {
+      <<-RUBY
+set :names,
+    last_name: "Smith"
+      RUBY
+    }
+
+    let(:level1) { Levels.read_ruby("the ruby", level1_ruby, "file.rb") }
+    let(:level2) { Levels.read_ruby("the ruby", level2_ruby, "file.rb") }
+
+    subject { Levels.merge(level1, level2) }
+
+    it "resolves the computed value" do
+      subject.names.full_name.must_equal "John Smith"
+    end
+  end
 end
