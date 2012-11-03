4784f6a6227aae5b8f58cdcabaca887c888cc142
Ryan Carver <ryan@ryancarver.com>
2012-11-03 14:57:50 -0700

    remove bad spec

--- test/unit/levels_test.rb
+++ test/unit/levels_test.rb
@@ -2,10 +2,6 @@ require 'helper'
 
 describe Levels do
 
-  specify ".new" do
-    Levels.new.must_be_instance_of Levels::Level
-  end
-
   specify ".merge" do
     Levels.merge([]).must_be_instance_of Levels::Merged
   end
