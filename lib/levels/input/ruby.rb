4ec59b04383553a30bca5409f4992bb7ac73069b
Ryan Carver <ryan@ryancarver.com>
2012-11-16 21:58:51 -0800

    spec and clarify the new ruby syntax

--- lib/levels/input/ruby.rb
+++ lib/levels/input/ruby.rb
@@ -15,27 +15,92 @@ module Levels
         dsl.close_current_group
       end
 
+      # The Ruby syntax DSL. This syntax aims to be very readable and writable.
+      # A stateful syntax and no trailing commas allows values to be easily
+      # moved around without excess text editing.
+      #
+      # Examples
+      #
+      #     group :webserver
+      #       set hostname: "localhost"
+      #     group :task_queue
+      #       set workers: 1
+      #       set queues: -> { ["high", "low", webserver.hostname] }
+      #
       class DSL
 
         def initialize(level)
           @level = level
         end
 
+        # Public: Start a group.
+        #
+        # name - Symbol or String name of the group.
+        #
+        # Returns nothing.
+        # Raises a RuntimeError if the group has already been defined.
         def group(name)
           close_current_group
+          if @level.defined?(name)
+            raise RuntimeError, "Group has already been created: #{name.inspect}"
+          end
           @group = name
           @hash = {}
+          nil
         end
 
-        def set(hash)
-          return if @hash.nil?
+        # Public: Set a key/value pair in the current group.
+        #
+        # Arguments
+        #
+        # hash  - Hash of key/value pairs. The keys may be a Symbol or String.
+        #
+        # key   - Symbol or String key.
+        # value - Any value.
+        #
+        # Examples
+        #
+        #     # Ruby 1.9 Hash.
+        #     set key: "value"
+        #
+        #     # Ruby 1.9 Hash.
+        #     set :key => "value"
+        #
+        #     # Key can be a String.
+        #     set "key" => "value"
+        #
+        #     # Multiple key/values.
+        #     set :key => "value", :more => "values"
+        #
+        #     # Key, value.
+        #     set :key, "value"
+        #
+        # Returns nothing.
+        # Raises SyntaxError if no current group is defined.
+        # Raises ArgumentError if other forms of arguments are given
+        # Raises RuntimeError if a key has already been set.
+        def set(*args)
+          raise SyntaxError, "No group is defined" if @hash.nil?
+          case
+          when args.size == 1 && Hash === args.first
+            hash = args.first
+          when args.size == 2 && !(args.any? { |a| Hash === a })
+            hash = { args.first => args.last }
+          else
+            raise ArgumentError, "Set must be given a Hash or two arguments. Got #{args.inspect}"
+          end
+          key_values = Levels::KeyValues.new(@hash)
+          hash.keys.each do |key|
+            if key_values.key?(key)
+              raise RuntimeError, "Key has already been set: #{key.inspect}"
+            end
+          end
           @hash.update(hash)
+          nil
         end
 
         def close_current_group
-          if @group && @hash
-            @level.set_group(@group, @hash)
-          end
+          @level.set_group(@group, @hash) if @group
           @group = nil
           @hash = nil
         end
--- test/unit/input/ruby_test.rb
+++ test/unit/input/ruby_test.rb
@@ -46,3 +46,95 @@ xgroup :foo
   end
 end
 
+describe Levels::Input::Ruby::DSL do
+
+  let(:level) { Levels::Level.new("Ruby") }
+
+  subject { Levels::Input::Ruby::DSL.new(level) }
+
+  def define(&block)
+    subject.instance_eval(&block)
+    subject.close_current_group
+  end
+
+  it "defines an empty group" do
+    define do
+      group :one
+    end
+    level.defined?(:one).must_equal true
+  end
+
+  it "defines a group with keys" do
+    define do
+      group :one
+      set key: "value"
+    end
+    level.one.key.must_equal "value"
+  end
+
+  it "is an error to set a key outside of a group" do
+    -> {
+      define do
+        set key: "value"
+      end
+    }.must_raise(SyntaxError)
+  end
+
+  it "is an error to set a key more than once" do
+    -> {
+      define do
+        group :one
+        set key: "value"
+        set "key" => "value"
+      end
+    }.must_raise(RuntimeError)
+  end
+
+  it "is an error to define a group more than once" do
+    -> {
+      subject.group :one
+      subject.close_current_group
+      subject.group "one"
+    }.must_raise(RuntimeError)
+  end
+
+  describe "the ways to set a value" do
+
+    before do
+      subject.group :one
+    end
+
+    def assert_key
+      subject.close_current_group
+      level.one.key.must_equal "value"
+    end
+
+    it "can be set as a ruby 1.9 hash" do
+      subject.set key: "value"
+      assert_key
+    end
+
+    it "can be set as a ruby 1.8 hash" do
+      subject.set :key => "value"
+      assert_key
+    end
+
+    it "is possible to set multiple keys via a hash" do
+      subject.set :key => "value", :bar => "foo"
+      assert_key
+      level.one.bar.must_equal "foo"
+    end
+
+    it "can be set with two arguments" do
+      subject.set :key, "value"
+      assert_key
+    end
+
+    it "is an error to pass other types of args" do
+      -> { subject.set :a }.must_raise(ArgumentError)
+      -> { subject.set :a, 1, :b }.must_raise(ArgumentError)
+      -> { subject.set :a, 1, :b, 2 }.must_raise(ArgumentError)
+      -> { subject.set :b, :a => 1 }.must_raise(ArgumentError)
+    end
+  end
+end
