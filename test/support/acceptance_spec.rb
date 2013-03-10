734c441894f8dfc9d99d8730fc7185f3706161ac
Ryan Carver <ryan@ryancarver.com>
2013-03-10 13:18:48 -0700

    update acceptance spec to expect Value object

--- test/support/acceptance_spec.rb
+++ test/support/acceptance_spec.rb
@@ -26,33 +26,33 @@ class AcceptanceSpec < MiniTest::Spec
   def self.assert_sample_data_set
 
     it "parses string" do
-      subject.types.string.must_equal "hello"
+      subject.types.string.must_equal Levels::Value.new("hello")
     end
 
     it "parses integer" do
-      subject.types.integer.must_equal 123
+      subject.types.integer.must_equal Levels::Value.new(123)
     end
 
     it "parses float" do
-      subject.types.float.must_equal 1.5
+      subject.types.float.must_equal Levels::Value.new(1.5)
     end
 
     it "parses boolean" do
-      subject.types.boolean_true.must_equal true
-      subject.types.boolean_false.must_equal false
+      subject.types.boolean_true.must_equal Levels::Value.new(true)
+      subject.types.boolean_false.must_equal Levels::Value.new(false)
     end
 
     it "parses array" do
-      subject.types.array_of_string.must_equal ["a", "b", "c"]
-      subject.types.array_of_integer.must_equal [1, 2, 3]
+      subject.types.array_of_string.must_equal Levels::Value.new(["a", "b", "c"])
+      subject.types.array_of_integer.must_equal Levels::Value.new([1, 2, 3])
     end
 
     it "parses null" do
-      subject.types.null.must_equal nil
+      subject.types.null.must_equal Levels::Value.new(nil)
     end
 
     it "parses all groups" do
-      subject.group2.message.must_equal "hello world"
+      subject.group2.message.must_equal Levels::Value.new("hello world")
     end
   end
 end
